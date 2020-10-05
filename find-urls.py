e180b8278a452926e9d73c96ea5b3f51a370fb85
neuronaddict <manufrommars@elacourarie.org>
2020-10-05 17:44:12 +0200

    Display with filename.

--- find-urls.py
+++ find-urls.py
@@ -1,4 +1,6 @@
-from androguard.core.analysis.analysis import Analysis, MethodAnalysis, StringAnalysis
+import os
+
+from androguard.core.analysis.analysis import Analysis, StringAnalysis
 from androguard.core.bytecodes.apk import APK
 from androguard.core.bytecodes.dvm import DalvikVMFormat
 from androguard.misc import AnalyzeAPK
@@ -7,6 +9,7 @@ import argparse
 parser = argparse.ArgumentParser('find string in a apk')
 parser.add_argument('apk')
 parser.add_argument('--only-login', help='display only logins', action='store_true')
+parser.add_argument('--show-file', help='Add filename before url', action='store_true')
 args = parser.parse_args()
 
 a: APK
@@ -15,11 +18,18 @@ dx: Analysis
 a, d, dx = AnalyzeAPK(args.apk)
 
 find_http = []
-find_login = []
+
 
 login_hints = ['login', 'auth', 'token', 'cred']
 
 exclude_domains = ['googleapis.com', 'crashlytics.com']
+
+
+def display(value, prefix=None):
+    if prefix:
+        print(prefix + ':' + value)
+    else:
+        print(value)
 
 
 for s in list(dx.get_strings()):
@@ -29,15 +39,10 @@ for s in list(dx.get_strings()):
     if 'http' in s.get_value() and all(t not in s.get_value() for t in exclude_domains):
         find_http.append(s.get_value())
 
-        for t in login_hints:
-            if t in s.get_value():
-                find_login.append(s.get_value())
-                break
-
         if not args.only_login:
-            print(s.get_value())
-
-for finded_login in find_login:
-    print(finded_login)
-
-
+            display(s.get_value(), os.path.basename(args.apk if args.show_file else None))
+        else:
+            for t in login_hints:
+                if t in s.get_value():
+                    display(s.get_value(), os.path.basename(args.apk if args.show_file else None))
+                    break
